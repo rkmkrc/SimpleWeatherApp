4dad62ed50428da863a7f1420ad30946d034caa8
erkam.karaca <erkamkaracaa@gmail.com>
2023-05-27 01:10:45 +0300

    Initial commit. Home page UI implemented. Network implemented.

---
+++ SimpleWeatherApp/SimpleWeatherApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Erkam Karaca on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleWeatherApp/SimpleWeatherApp/Model/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  SimpleWeatherApp
+//
+//  Created by Erkam Karaca on 26.05.2023.
+//
+
+import Foundation
+
+struct Constants {
+    let placeholderCityName = "No city info."
+    let placeholderTemperature = "No temperature info."
+    let placeholderHumidity = "No humidity info."
+    let placeholderSpeed = "No speed info."
+    let placeholderGust = "No gust info."
+    let placeholderSeaLevel = "No sea level info."
+    let placeholderLon = "No longitude info."
+    let placeholderLat = "No latitude info."
+    let placeholderWeatherDescription = "No weather description info."
+}
